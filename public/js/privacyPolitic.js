3331b11e6fd8717c589f066b1e928343cd8f6898
CarlosGil <carlosgdaries@gmail.com>
2025-09-16 18:08:01 +0200

    cambio en los estilos de los textos dinámicos de las traducciones

--- public/js/privacyPolitic.js
+++ public/js/privacyPolitic.js
@@ -7,25 +7,25 @@ const categoriesDropDown = document.getElementById('categories');
 const gendersDropDown = document.getElementById('genders');
 
 async function loadPrivacyPolitic() {
-    const response = await fetch('/api/privacy-politic');
-    const data = await response.json();
+  const response = await fetch('/api/privacy-politic');
+  const data = await response.json();
 
-    data.privacyPolitics.forEach(item => {
-      const titleContainer = document.createElement('div');
-      titleContainer.classList.add('title');
-      titleContainer.setAttribute(
-        'data-i18n',
-        `privacy_politic_${item.id}_title`
-      );
-      const textContainer = document.createElement('div');
-      textContainer.classList.add('text');
-      textContainer.setAttribute(
-          'data-i18n',
-          `privacy_politic_${item.id}_text`
-        );
-      container.appendChild(titleContainer);
-      container.appendChild(textContainer);
-    });
+  data.privacyPolitics.forEach((item) => {
+    const section = document.createElement('section');
+    section.classList.add('mb-5');
+    const titleContainer = document.createElement('h2');
+    titleContainer.classList.add('title', 'h4', 'fw-bold', 'mb3');
+    titleContainer.setAttribute(
+      'data-i18n',
+      `privacy_politic_${item.id}_title`
+    );
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text', 'mb3');
+    textContainer.setAttribute('data-i18n', `privacy_politic_${item.id}_text`);
+    section.appendChild(titleContainer);
+    section.appendChild(textContainer);
+    container.appendChild(section);
+  });
 }
 
 dropDownTypeMenu(categoriesDropDown, 'categories', 'category');
